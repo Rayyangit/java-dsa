bd716dda69322b8996b7908f9d180df4280b6cde
Rayyan <rayyanmulla128@gmail.com>
2025-02-21 19:57:00 +0530

    done

--- src/greater.java
+++ src/greater.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class greater {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,10};
+        int ans=maximum(arr);
+        System.out.println(ans);
+    }
+    public static int maximum(int[] arr){
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<arr.length;i++){
+           max = arr[i];
+       }
+       return max;
+    }
 }
